fa9475681cc8292821891a3cbad64a8e688f9cdf
Javi Palacios <javi@fjp.es>
2019-07-27 23:31:47 +0200

    Switched from  promises to an async/await way

--- routes/movies.js
+++ routes/movies.js
@@ -16,64 +16,55 @@ router
       res.status(404).json({ message: `Error: ${message}` });
     }
   })
-  .get('/', (req, res) => {
-    const movies = MovieModel.find().sort({ _id: -1 });
-    movies
-      .then((result) => {
-        if (result.length === 0) {
-          return res.status(404).send({ message: 'No movies found' });
-        }
-        return res.status(200).send({ movies: result });
-      })
-      .catch((error) => {
-        res.status(404).json({ error });
-      });
+  .get('/', async (req, res) => {
+    try {
+      const movies = await MovieModel.find().sort({ _id: -1 });
+      if (movies.length === 0) {
+        return res.status(404).send({ message: 'No movies found' });
+      }
+      return res.status(200).send({ movies });
+    } catch (error) {
+      return res.status(404).json({ error });
+    }
   })
-  .get('/:id', (req, res) => {
-    const { id } = req.params;
-    const movie = MovieModel.findById(id);
-    movie
-      .then((result) => {
-        if (result.length === 0) {
-          return res.status(404).send({ message: 'No movie found' });
-        }
-        return res.status(200).send(result);
-      })
-      .catch((error) => {
-        res.status(404).json({ error });
-      });
+  .get('/:id', async (req, res) => {
+    try {
+      const { id } = req.params;
+      const movie = await MovieModel.findById(id);
+      if (movie.length === 0) {
+        return res.status(404).send({ message: 'No movie found' });
+      }
+      return res.status(200).send(movie);
+    } catch (error) {
+      return res.status(404).json({ error });
+    }
   })
-  .put('/:id', authorization, (req, res) => {
-    const { id } = req.params;
-    const newMovie = req.body;
-    const updatedMovie = MovieModel.findOneAndUpdate(id, newMovie);
-    updatedMovie
-      .then((movie) => {
-        if (movie.isModified) {
-          return res.status(200).send({
-            message: 'Movie successfully updated',
-            movie,
-          });
-        }
-        return res.status(500).send({ message: 'Request error' });
-      })
-      .catch((error) => {
-        res.status(500).json({ error });
-      });
+  .put('/:id', authorization, async (req, res) => {
+    try {
+      const { id } = req.params;
+      const newMovie = req.body;
+      const movie = await MovieModel.findOneAndUpdate(id, newMovie);
+      if (movie.isModified) {
+        return res.status(200).send({ message: 'Movie successfully updated' });
+      }
+      return res.status(500).send({ message: 'Request error' });
+    } catch (error) {
+      return res.status(500).json({ error });
+    }
   })
-  .delete('/:id', authorization, (req, res) => {
-    const { id } = req.params;
-    const deletedMovie = MovieModel.findOneAndDelete(id);
-    deletedMovie
-      .then((movie) => {
-        if (movie.isModified) {
-          return res.status(200).send({ message: 'Movie successfully deleted' });
-        }
-        return res.status(500).send({ message: 'Request error' });
-      })
-      .catch((error) => {
-        res.status(500).json({ error });
-      });
+  .delete('/:id', authorization, async (req, res) => {
+    try {
+      const { id } = req.params;
+      const movie = await MovieModel.findOneAndDelete(id);
+      if (movie.isModified) {
+        return res
+          .status(200)
+          .send({ message: 'Movie successfully deleted' });
+      }
+      return res.status(500).send({ message: 'Request error' });
+    } catch (error) {
+      return res.status(500).json({ error });
+    }
   });
 
 module.exports = router;

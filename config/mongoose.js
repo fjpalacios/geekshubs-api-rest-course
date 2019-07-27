a4e6289f6a78b092248b921ee75d8b62babf15d4
Javi Palacios <javi@fjp.es>
2019-07-27 19:40:46 +0200

    Added Movie CRUD

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,6 +4,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/movies-avantio', {
   useNewUrlParser: true,
   useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 module.exports = mongoose;
--- routes/movies.js
+++ routes/movies.js
@@ -1,14 +1,79 @@
 const express = require('express');
 const authorization = require('../middlewares/authorization');
+const MovieModel = require('../models/movie');
 
 const router = express.Router();
 
 router
-  .get('/', (req, res) => {
-    res.json({ message: 'Movies' });
+  .post('/', authorization, async (req, res) => {
+    try {
+      const newMovie = req.body;
+      const movie = new MovieModel(newMovie);
+      await movie.save();
+      res.json({ message: 'New movie saved', movie });
+    } catch (error) {
+      const message = error.message || error.errmsg;
+      res.status(404).json({ message: `Error: ${message}` });
+    }
   })
-  .post('/', authorization, (req, res) => {
-    res.json({ message: 'Movies' });
+  .get('/', (req, res) => {
+    const movies = MovieModel.find().sort({ _id: -1 });
+    movies
+      .then((result) => {
+        if (result.length === 0) {
+          return res.status(404).send({ message: 'No movies found' });
+        }
+        return res.status(200).send({ movies: result });
+      })
+      .catch((error) => {
+        res.status(404).json({ error });
+      });
+  })
+  .get('/:id', (req, res) => {
+    const { id } = req.params;
+    const movie = MovieModel.findById(id);
+    movie
+      .then((result) => {
+        if (result.length === 0) {
+          return res.status(404).send({ message: 'No movie found' });
+        }
+        return res.status(200).send(result);
+      })
+      .catch((error) => {
+        res.status(404).json({ error });
+      });
+  })
+  .put('/:id', authorization, (req, res) => {
+    const { id } = req.params;
+    const newMovie = req.body;
+    const updatedMovie = MovieModel.findOneAndUpdate(id, newMovie);
+    updatedMovie
+      .then((movie) => {
+        if (movie.isModified) {
+          return res.status(200).send({
+            message: 'Movie successfully updated',
+            movie,
+          });
+        }
+        return res.status(500).send({ message: 'Request error' });
+      })
+      .catch((error) => {
+        res.status(500).json({ error });
+      });
+  })
+  .delete('/:id', authorization, (req, res) => {
+    const { id } = req.params;
+    const deletedMovie = MovieModel.findOneAndDelete(id);
+    deletedMovie
+      .then((movie) => {
+        if (movie.isModified) {
+          return res.status(200).send({ message: 'Movie successfully deleted' });
+        }
+        return res.status(500).send({ message: 'Request error' });
+      })
+      .catch((error) => {
+        res.status(500).json({ error });
+      });
   });
 
 module.exports = router;
